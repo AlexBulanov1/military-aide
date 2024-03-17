05ffe78682b32da05121b25dcf53b96f142e668f
AlexBulanov <vkbulanov@icloud.com>
2024-03-17 21:27:34 +0200

    Add not found page

--- src/pages/NotFound.tsx
+++ src/pages/NotFound.tsx
@@ -1,5 +1,16 @@
+import { Link } from 'react-router-dom';
+
 const NotFound = () => {
-	return <div>NotFound</div>;
+	return (
+		<h1 className='text-center font-bold text-5xl'>
+			Oops... <br />
+			The page you are looking for was not found
+			<br />
+			<Link className='underline' to={'/'}>
+				Navigate to the home page
+			</Link>
+		</h1>
+	);
 };
 
 export default NotFound;
